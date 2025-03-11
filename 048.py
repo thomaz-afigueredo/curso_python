98f2b90c8e4d7f03e558d7b49938f0fa870b2946
thomaz-afigueredo <thomaz.afigueredo@gmail.com>
2025-03-11 16:47:53 -0300

    ex 46 ao 49

--- 048.py
+++ 048.py
@@ -1,1 +1,6 @@
 # Exercício Python 048: Faça um programa que calcule a soma entre todos os números que são múltiplos de três e que se encontram no intervalo de 1 até 500.
+soma = 0
+for c in range (0,501):
+    if c % 3 == 0:
+        soma = soma + c
+print(soma)
--- 049.py
+++ 049.py
@@ -1,1 +1,4 @@
 # Exercício Python 049: Refaça o DESAFIO 009, mostrando a tabuada de um número que o usuário escolher, só que agora utilizando um laço for.
+num = float(input('Qual tabuada de multiplicação vamos ver hoje?'))
+for c in range (0,11):
+    print(num, c , num * c)
