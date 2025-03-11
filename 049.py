f93facbb85309c405edd2903224d78496ac8ecf6
thomaz-afigueredo <thomaz.afigueredo@gmail.com>
2025-03-11 19:46:38 -0300

    ex 49 e 50

--- 049.py
+++ 049.py
@@ -1,4 +1,6 @@
 # Exercício Python 049: Refaça o DESAFIO 009, mostrando a tabuada de um número que o usuário escolher, só que agora utilizando um laço for.
-num = float(input('Qual tabuada de multiplicação vamos ver hoje?'))
+num = int(input('Qual tabuada de multiplicação vamos ver hoje? '))
+print('-=-'*4)
 for c in range (0,11):
-    print(num, c , num * c)
+    print('{} x {:2} = {:2}'.format(num, c , num * c))
+print('-=-'*4)
--- 050.py
+++ 050.py
@@ -1,1 +1,5 @@
 # Exercício Python 050: Desenvolva um programa que leia seis números inteiros e mostre a soma apenas daqueles que forem pares. Se o valor digitado for ímpar, desconsidere-o.
+n = 0
+for c in range (0,6):
+    n = int(input('Insira o número: '))
+print(n)
