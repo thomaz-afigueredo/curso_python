f7055a67a710b529c50af250607c2e56b8eb6490
thomaz-afigueredo <thomaz.afigueredo@gmail.com>
2025-03-11 22:01:23 -0300

    ex 46 - ex 52

--- 047_contagem_de_pares.py
+++ 047_contagem_de_pares.py
@@ -1,4 +1,4 @@
 # Exercício Python 047: Crie um programa que mostre na tela todos os números pares que estão no intervalo entre 1 e 50.
-for c in range (0 , 51):
-    if c % 2 == 0:
-        print(c, end= ' ')
+for c in range (2 , 51 , 2):
+    print('.' , end='')
+    print(c, end= ' ')
--- 051.py
+++
@@ -1,1 +0,0 @@
-# Exercício Python 051: Desenvolva um programa que leia o primeiro termo e a razão de uma PA. No final, mostre os 10 primeiros termos dessa progressão.
