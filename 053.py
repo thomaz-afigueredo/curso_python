f2328a9b1c374aba6ff340b7e12669b4d121f82a
thomaz-afigueredo <thomaz.afigueredo@gmail.com>
2025-03-13 09:53:18 -0300

    53

--- 053.py
+++ 053.py
@@ -1,1 +1,5 @@
 # Exercício Python 053: Crie um programa que leia uma frase qualquer e diga se ela é um palíndromo, desconsiderando os espaços.
+f = input('Insira uma frase: ').strip().lower()
+fseparado = f.split()
+fjunto = ''.join(fseparado)
+print(fjunto)
